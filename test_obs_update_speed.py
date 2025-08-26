31c939ff3bbc12147ed00b2681179bde08bda61b
WS P <38765016+Phoenix465@users.noreply.github.com>
2025-08-26 23:45:32 +0100

    Initial commit

---
+++ test_obs_update_speed.py
@@ -0,0 +1,15 @@
+from time import sleep
+import obsws_python as obs
+
+# pass conn info if not in config.toml
+cl = obs.ReqClient(host='localhost', port=4455, password='cTSydoJdOA7zNhz0', timeout=3)
+resp = cl.get_version()
+print(f"OBS Version: {resp.obs_version}")
+
+
+i = 0
+while True:
+    sleep(0.05)
+    i += 1
+    cl.set_input_settings("Points", {"text": str(i)}, True)
+
